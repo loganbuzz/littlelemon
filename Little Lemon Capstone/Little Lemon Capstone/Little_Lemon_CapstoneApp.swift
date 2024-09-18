cb8f2995e640f0b707f8c8d516312d98bd653dd9
loganbuzz <loganbuzzard91@gmail.com>
2024-09-18 18:51:59 -0400

    Initial commit

---
+++ Little Lemon Capstone/Little Lemon Capstone/Little_Lemon_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_CapstoneApp.swift
+//  Little Lemon Capstone
+//
+//  Created by Logan Buzzard on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
